a5542f0286305f405829051b3d8ae2920c5e2388
Wiktor <wiktor.szczeszek99.other@gmail.com>
2022-12-20 22:46:42 +0100

    Aktualizacja parametru null dla tabel

--- Backend/SQL/Tables/ConstellationsTable.sql
+++ Backend/SQL/Tables/ConstellationsTable.sql
@@ -2,6 +2,6 @@ CREATE TABLE [Constellations]
 (
     [Id] INT IDENTITY(1,1) PRIMARY KEY,
     [Name] VARCHAR(255) NOT NULL,
-    [Description] VARCHAR(MAX),
+    [Description] VARCHAR(MAX) NULL,
     [Link] VARCHAR(255) NULL
 );
