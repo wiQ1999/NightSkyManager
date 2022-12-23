a5eeb6407820f9bc9a427c6fedd09e271d4aa2aa
Wiktor <wiktor.szczeszek99.other@gmail.com>
2022-12-23 18:00:42 +0100

    Dodanie generatora zapytań dla tablicy gwiazd

---
+++ Backend/database/queries/starsQueries.js
@@ -0,0 +1,38 @@
+const baseQueries = require('./baseQueries');
+
+const table = 'stars';
+
+function selectAllStars() {
+    return baseQueries.selectAll(table);
+}
+
+function selectStarById(id) {
+    return baseQueries.selectById(table, id);
+}
+
+function selectConstellationsByStarId(id) {
+    return `SELECT [constellations].* FROM [constellations] 
+        INNER JOIN [constellations_stars] AS [sc] ON [sc].[constellationId]=[constellations].[id] 
+        WHERE [sc].[starsId]=${id};`
+}
+
+function insertStar(dto) {
+    return baseQueries.insert(table, dto);
+}
+
+function updateStarById(id, dto) {
+    return baseQueries.updateById(table, id, dto);
+}
+
+function deleteStarnById(id) {
+    return baseQueries.deleteById(table, id);
+}
+
+module.exports = {
+    selectAllStars,
+    selectStarById,
+    selectConstellationsByStarId,
+    insertStar,
+    updateStarById,
+    deleteStarnById
+};
