b14b09efd120107bbb42eafc0a3c8c8cc8ad9f20
Wiktor <wiktor.szczeszek99.other@gmail.com>
2022-12-23 18:20:38 +0100

    Aktualziacja pliku app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,97 +1,11 @@
 const express = require('express');
 const constellationsRouter = require('./controllers/constellationsController');
-const {
-    selectAllStars,
-    selectById,
-    insertStar,
-    updateStar,
-    deleteStar
-} = require('./commands/starsCommands');
+const starsRouter = require('./controllers/starsController');
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
-
 app.use('/constellations', constellationsRouter);
-
-app.post('/stars', async (req, res) => {
-    try {
-        res.send(await insertStar(req.body));
-    } catch (error) {
-        res.status(400).send(error.field);
-    }
-});
-
-app.get('/stars', async (req, res) => res.send(await selectAllStars()));
-
-app.get('/stars/:id', async (req, res) => {
-    const id = parseInt(req.params.id);
-    if (!id) {
-        res.status(400).send("Missing 'id' parameter.");
-        return;
-    }
-
-    try {
-        res.send(await selectById(id));
-    } catch (error) {
-        res.status(400).send(error.field);
-    }
-});
-
-app.get('/stars/:id/constellations', async (req, res) => {
-    const id = parseInt(req.params.id);
-    if (!id) {
-        res.status(400).send("Missing 'id' parameter.");
-        return;
-    }
-
-    try {
-        const star = await selectById(id);
-
-    } catch (error) {
-        res.status(400).send(error.field);
-    }
-});
-
-app.put('/stars/:id', async (req, res) => {
-    const id = parseInt(req.params.id);
-    if (!id) {
-        res.status(400).send("Missing 'id' parameter.");
-        return;
-    }
-
-    const body = req.body;
-    if (!body) {
-        res.status(400).send("Missing request body.");
-        return;
-    }
-
-    try {
-        res.send(await updateStar(id, body));
-    } catch (error) {
-        res.status(400).send(error.field);
-    }
-});
-
-app.delete('/stars/:id', async (req, res) => {
-    const id = parseInt(req.params.id);
-    if (!id) {
-        res.status(400).send("Missing 'id' parameter.");
-        return;
-    }
-
-    const body = req.body;
-    if (!body) {
-        res.status(400).send("Missing request body.");
-        return;
-    }
-
-    try {
-        res.send(await deleteStar(id, body));
-    } catch (error) {
-        res.status(400).send(error.field);
-    }
-});
-
+app.use('/stars', starsRouter);
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
