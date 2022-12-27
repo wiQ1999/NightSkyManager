b306cfe10bc92d099adccc6eb5faa489210231c6
Wiktor <wiktor.szczeszek99.other@gmail.com>
2022-12-27 21:25:08 +0100

    Dodanie biblioteki cors do Web API

--- Backend/app.js
+++ Backend/app.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const cors = require('cors');
 const constellationsRouter = require('./controllers/constellationsController');
 const starsRouter = require('./controllers/starsController');
 
 const app = express();
+const corsOptions = {
+    origin: '*',
+    credentials: true,
+    optionSuccessStatus: 200,
+};
 const port = 3000;
 
 app.use(express.json());
+app.use(cors(corsOptions));
+
 app.use('/constellations', constellationsRouter);
 app.use('/stars', starsRouter);
 app.listen(port, () => console.log(`NightSkyManager Web API created by Wiktor Szczeszek listening at http://localhost:${port}`));
