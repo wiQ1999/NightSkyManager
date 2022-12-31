fd178bd3214995dfa96a7dc35690e5f4094eee5c
Wiktor <wiktor.szczeszek99.other@gmail.com>
2022-12-31 12:48:07 +0100

    Aktualizacja strony startowej

--- Frontend/src/routes/+page.svelte
+++ Frontend/src/routes/+page.svelte
@@ -1,10 +1,33 @@
 <svelte:head>
-	<title>Home</title>
-	<!-- <meta name="description" content="" /> -->
+	<title>NightSkyManager</title>
 </svelte:head>
 
 <section>
-	<h1>Home</h1>
+	<h1>Aboute</h1>
+
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo iure error
+		provident possimus suscipit nesciunt doloribus deserunt consequatur
+		voluptates! Totam quas quasi delectus temporibus culpa eum
+		necessitatibus vero, asperiores modi! Lorem ipsum dolor sit amet
+		consectetur adipisicing elit. Blanditiis, labore? Assumenda illo itaque
+		animi fugiat consequatur ipsam esse, praesentium, expedita culpa
+		deleniti voluptatem, quo aspernatur voluptate repellat iure sed soluta?
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe ut,
+		perferendis iure eaque voluptate eos, animi repellendus doloribus
+		molestias consectetur maxime sequi. Neque, labore? Officiis hic debitis
+		vero ipsam quia!
+	</p>
+
+	<p>
+		Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil, illum
+		sequi distinctio nobis aliquam temporibus repellendus repudiandae
+		placeat facilis quaerat minus quam, labore similique, fuga pariatur
+		ratione sunt modi. Aspernatur? Lorem, ipsum dolor sit amet consectetur
+		adipisicing elit. Nulla nam fugit ab iusto omnis. Illum repellendus
+		inventore optio velit corrupti! Odio nisi repudiandae dolorem debitis,
+		nemo quod non culpa commodi?
+	</p>
 </section>
 
 <style>
