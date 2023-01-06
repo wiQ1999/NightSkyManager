cd2f852ebc982ba1446c501662f9bdbdef5140d0
Wiktor <wiktor.szczeszek99.other@gmail.com>
2023-01-06 13:01:07 +0100

    Rozszerzenie seedów bazy danych

--- Backend/database/seeds/constellations_starsSeed.sql
+++ Backend/database/seeds/constellations_starsSeed.sql
@@ -2,6 +2,12 @@ INSERT INTO [constellations_stars] ([constellationId], [starId]) VALUES
 (1, 1),
 (1, 2),
 (1, 3),
-(1, 4),
-(1, 5),
-(1, 6)
+(2, 4),
+(2, 5),
+(2, 6),
+(3, 7),
+(3, 8),
+(3, 9),
+(4, 10),
+(4, 11),
+(4, 12)
